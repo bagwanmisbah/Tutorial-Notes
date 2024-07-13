5862f86ea7c45472aa81d82c7802d15ae8e06dee
Misbah Bagwan <162052331+bagwanmisbah@users.noreply.github.com>
2024-07-13 18:00:03 +0530

    Create 1_Default_param_constructor.cpp
    WAP to demonstrate default parameterized constructor

---
+++ OOP-Insem-Theory/1_Default_param_constructor.cpp
@@ -0,0 +1,36 @@
+//WAP TO DEMONSTRATE DEFAULT PARAMETERISED CONSTRUCTOR 
+#include <iostream>
+#include <string>
+using namespace std;
+class emp
+{
+    int eid;
+    float sal;
+    string name;
+    int age;
+    public:
+    emp(){} //default constructor 
+    emp(int e,float s,string n,int a=30)  //parameterized 
+    {
+        eid=e;
+        sal=s;
+        name=n;
+        age=a;
+    }
+    void put()
+    {
+        cout<<eid<<endl;
+        cout<<sal<<endl;
+        cout<<name<<endl;
+        cout<<age<<endl;
+    }
+};
+int main()
+{
+    emp e1;
+    e1=emp(5,250.0,"ABC");
+    e1.put();
+    emp e2(4,900.0, "DEF"); //constructor with default arguments
+    e2.put();
+    return 0;
+}
