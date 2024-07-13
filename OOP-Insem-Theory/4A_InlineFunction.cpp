d77b134501561ed2c284851709683ec410cfe7d0
Misbah Bagwan <162052331+bagwanmisbah@users.noreply.github.com>
2024-07-14 00:14:11 +0530

    Create 4A_InlineFunction.cpp

---
+++ OOP-Insem-Theory/4A_InlineFunction.cpp
@@ -0,0 +1,23 @@
+//AUTHOR :  MISBAH BAGWAN 21487
+//WAP to demonstrate default parameterised constructor
+#include<iostream>
+using namespace std;
+class number
+{
+    public:
+    inline double mult(double a,double b)
+    {
+        return a*b;
+    }
+    inline int cube(int a)
+    {
+        return a*a*a;
+    }
+};
+int main()
+{
+    number n;
+    cout<<n.mult(12,12)<<endl;
+    cout<<n.cube(2)<<endl;
+return 0;
+}
