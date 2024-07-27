aef3e52264b2c4483180181a45e2478a3084106a
Misbah Bagwan <162052331+bagwanmisbah@users.noreply.github.com>
2024-07-27 18:24:50 +0530

    Create 15_MethodOverriding.cpp

---
+++ OOP-Insem-Theory/15_MethodOverriding.cpp
@@ -0,0 +1,30 @@
+//METHOD OVERRIDING
+#include <iostream>
+using namespace std;
+class A
+{
+    public:
+    int a=100,b=5;
+    void put()
+    {
+        cout<<"From base class : "<<a*b<<endl;
+    }
+};
+class B:public A
+{
+    public:
+    void put()
+    {
+        cout<<"From derived class : "<<a/b<<endl;
+    }
+};
+int main()
+{
+    A a;
+    B b;
+    a.put();
+    b.put();
+    return 0;
+}
+From base class : 500
+From derived class : 20
